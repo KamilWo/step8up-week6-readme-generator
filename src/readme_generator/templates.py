66d04bdfe8012f19e772a29b2f1bb622b16b9c8d
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-07-14 18:09:10 +0100

    Second version, improved

--- src/readme_generator/templates.py
+++ src/readme_generator/templates.py
@@ -2,54 +2,98 @@
 Template functions.
 """
 
-def template_python(  # pylint: disable=too-many-arguments,too-many-positional-arguments
-    project_title: str,
-    description: str,
-    installation: str,
-    usage: str,
-    license_choice: str,
-    author_name: str,
-    contact_info: str,
-):
+
+def template_python(user_data: dict[str, str]):
     """
     Returns README template for Python project.
 
-    :param project_title: Project Title.
-    :param description: Description.
-    :param installation: Installation.
-    :param usage: Usage.
-    :param license_choice: License choice.
-    :param author_name: Author name.
-    :param contact_info: Contact info.
+    :param user_data: Data gathered from the user in form of a dict.
 
     :return: README template.
     """
 
-    return f"""# {project_title}
+    return f"""# {user_data["project_title"]}
 
 ## Description
-{description}
+{user_data["description"]}
+
+---
+
+## Python installation
+
+- Ensure that a recent version of Python 3 is installed on your system (3.10+).
+- If not, then install Python3 based on the official
+  documentation [Setup and Usage](https://docs.python.org/3.13/using/index.html)
 
 ---
 
 ## Installation
-{installation}
+{user_data["installation"]}
 
 ---
 
 ## Usage
-{usage}
+{user_data["usage"]}
 
 ---
 
 ## License
-This project is licensed under the `{license_choice}`. Please see the `LICENSE` file.
+This project is licensed under the `{user_data["license_choice"]}`. 
+Please see the `LICENSE` file.
 
 ---
 
 ## Author
-{author_name}
+{user_data["author_name"]}
 
-{contact_info.replace("\n", "\n\n")}
+{user_data["contact_info"]}
+
+"""
+
+
+def template_nodejs(user_data: dict[str, str]):
+    """
+    Returns README template for NOde.js project.
+
+    :param user_data: Data gathered from the user in form of a dict.
+
+    :return: README template.
+    """
+
+    return f"""# {user_data["project_title"]}
+
+## Description
+{user_data["description"]}
+
+---
+
+## Node.js installation
+
+- Ensure that a recent version of Node.js is installed on your system (18+).
+- If not, then install Node.js based on the official
+  documentation [Downloading and Installing](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm)
+
+---
+
+## Installation
+{user_data["installation"]}
+
+---
+
+## Usage
+{user_data["usage"]}
+
+---
+
+## License
+This project is licensed under the `{user_data["license_choice"]}`. 
+Please see the `LICENSE` file.
+
+---
+
+## Author
+{user_data["author_name"]}
+
+{user_data["contact_info"]}
 
 """
